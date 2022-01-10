6f2dcf677eb1a21dac516d3beea56721d618f1be
Ethan Isola <isolaethan@gmail.com>
2022-01-10 11:50:42 -0600

    commit tree 8e5f7a9761ef7c5cafcccae0ee4af6a901cb3f26

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(length){
   // Write your code here!
+  if(length <= 400){
+    return 'This one is on me!';
+  }
+  else if(length > 2000 && length < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(length > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let cityCheck;
+  city == 'NYC' ? (cityCheck = "Ok, sounds good.") : (cityCheck = "No go.");  
+  return cityCheck;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
